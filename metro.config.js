ada9d8c3a7f8e77d84ea7842458a672665f0df87
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-10-10 19:23:41 +0100

    init

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+/**
+ * @format
+ */
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
